c5f292f3e61a7e2b30d09d8c847df35efed5b6a4
jbarr <jbarr@SOLUS>
2016-10-10 10:55:15 +0100

    Initial commit

---
+++ CustomerPortal/CustomerPortal.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class CustomerPortal
+    Inherits System.Web.UI.Page
+    Private NCSoapClient As NCWebAPI.ProductsAndOrderingSoapClient
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        NCSoapClient = New NCWebAPI.ProductsAndOrderingSoapClient()
+
+    End Sub
+
+End Class
